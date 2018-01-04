db7094702d60fce5852652ce802e807c3845e7f0
Michael Evans <michael@michaelgregoryevans.com>
2018-01-04 05:22:22 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
-  def self.current_user(session_hash)
+  def self.current_user(session)
     binding.pry
-    User.find_by(session_hash[:user_id])
+    User.find_by(id: session[:user_id])
   end
 
-  def self.is_logged_in?(session_hash)
-    !!session_hash[:user_id]
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
 end
